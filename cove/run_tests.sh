f929afb5b7f3cd3179edb68a64ad95901d4b2e1f
Michael Wood <michael.wood@opendataservices.coop>
2021-05-13 20:24:49 +0100

    pytest: Update django settings paths

--- cove/run_tests.sh
+++ cove/run_tests.sh
@@ -1,1 +1,1 @@
-DJANGO_SETTINGS_MODULE=cove_360.settings py.test cove_360 --cov-append --cov --cov-report= $@
+DJANGO_SETTINGS_MODULE=cove_project.settings py.test cove_360 --cov-append --cov --cov-report= $@
