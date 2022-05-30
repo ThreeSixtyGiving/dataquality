35492836c025b7507ee37cabd087df75f4836dc4
Michael Wood <michael.wood@opendataservices.coop>
2022-05-30 21:41:44 +0100

    WIP

--- cove/cove_360/models.py
+++ cove/cove_360/models.py
@@ -9,13 +9,18 @@ from urllib.parse import urlparse
 class SuppliedDataStatus(models.Model):
     supplied_data = models.ForeignKey(SuppliedData, on_delete=models.CASCADE)
     passed = models.BooleanField(default=False)
-    publisher = models.TextField(null=True, blank=True)
+    _publisher = models.TextField(null=True, blank=True)
+
+    @property
+    def publisher(self):
+        if self._publisher:
+            return json.loads(self._publisher)
+        return None
 
     @property
     def can_publish(self):
         if self.publisher:
-            publisher = json.loads(self.publisher)
-            return (publisher["self_publish"]["enabled"] and self.passed)
+            return (self.publisher["self_publish"]["enabled"] and self.passed)
 
         return False
 
@@ -34,7 +39,7 @@ class SuppliedDataStatus(models.Model):
 
         for publisher in publishers.values():
             if data_domain in publisher["self_publish"]["authorised_domains"]:
-                self.publisher = json.dumps(publisher)
+                self._publisher = json.dumps(publisher)
                 break
 
         super(SuppliedDataStatus, self).save(*args, **kwargs)
