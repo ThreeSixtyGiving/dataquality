4fce6e1319885f70249c9cbb8d9b4243538794cb
Michael Wood <michael.wood@opendataservices.coop>
2022-07-18 15:50:17 +0100

    WIP

--- cove/cove_360/publishing.py
+++ cove/cove_360/publishing.py
@@ -1,11 +1,13 @@
 from requests.exceptions import HTTPError
+from requests.auth import HTTPBasicAuth
 import requests
 import os
-import json
 from urllib.parse import urlparse
+
 
 def extract_domain(source_url):
     return urlparse(source_url).netloc
+
 
 def lookup_publisher_by_domain(source_url):
     """Looks up to see if we have a matching domain for source_url in the publisher
@@ -16,23 +18,22 @@ def lookup_publisher_by_domain(source_url):
         "https://data.threesixtygiving.org/publishers.json",
     )
 
-    try:
-        r = requests.get(registry_url)
-        r.raise_for_status()
+    basic = HTTPBasicAuth(
+        os.environ.get("REGISTRY_PUBLISHERS_USER", ""),
+        os.environ.get("REGISTRY_PUBLiSHERS_PASS", ""),
+    )
+    r = requests.get(registry_url, auth=basic)
+    r.raise_for_status()
 
-        publishers = r.json()
+    publishers = r.json()
 
-        data_domain = extract_domain(source_url)
+    data_domain = extract_domain(source_url)
 
-        for publisher in publishers.values():
-            if data_domain in publisher["self_publish"]["authorised_domains"]:
-                return publisher
+    for publisher in publishers.values():
+        if data_domain in publisher["self_publish"]["authorised_domains"]:
+            return publisher
 
-        if not publisher:
-            print("Domain name for publisher could not be found")
-
-    except HTTPError:
-        print("Failed to contact registry to verify publisher domain")
-        pass
+    if not publisher:
+        print("Domain name for publisher could not be found")
 
     return None
