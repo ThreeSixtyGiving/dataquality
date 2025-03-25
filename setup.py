fb3b353331bae1baa60f116ab44657da8cc4a1eb
Michael Wood <michael.wood@opendataservices.coop>
2025-03-25 12:30:00 +0000

    requirements_cove: upgrade all requirements
    This is for the django upgrade for the python 3.12 upgrade

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     ],
     classifiers=[
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.12',
     ],
 )
